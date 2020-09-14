80f4bae67417f354aa53aadd41ada9705b08fb46
SudKul <sudhanshu.kulshrestha@gmail.com>
2020-09-14 22:32:02 +0530

    Add solution to Creating 3D-Objects exercise

--- Combining Camera and Lidar/Creating 3D-Objects/cluster_with_roi/src/cluster_with_roi.cpp
+++ Combining Camera and Lidar/Creating 3D-Objects/cluster_with_roi/src/cluster_with_roi.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff9012880eb4d05e7b37c0fe560da4851ff86d0047cab43a91dfa87e91f80c6d
-size 6541
+oid sha256:e3855f4fbe112f7ee4918fdbb12e7c1bde5f3499dd318be9e6b65fe16ad56f43
+size 6731
---
+++ Combining Camera and Lidar/Creating 3D-Objects/solution/clusterLidarWithROI.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8f1a030a67fc643a6c9bb594228cb3b8787cf80e8f0707039e5aa33079dc3450
+size 2276
