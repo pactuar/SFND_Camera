59f1b646c167974d4482402c88c2b6b29715bcec
pactuar <adam.cowley@packsize.com>
2025-01-06 16:50:08 -0700

    first exercises completed

--- Lesson 2 - Autonomous Vehicles and Computer Vision/The OpenCV Library/OpenCV_exercises/src/create_matrix.cpp
+++ Lesson 2 - Autonomous Vehicles and Computer Vision/The OpenCV Library/OpenCV_exercises/src/create_matrix.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4d2e5825a475812e15d0a89830bd6d34db8a329499c01028bc68b1d7d3a0c2e1
-size 906
+oid sha256:ced686ca02642ac58b92fddf9e921d2f28473d8a8a72629fa17f3a6170fbaa01
+size 1212
