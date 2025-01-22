413a6cb734501058cfd445b0b01a353282e931d7
pactuar <adam.cowley@packsize.com>
2025-01-22 09:58:01 -0700

    finished last camera lesson kinda

--- Lesson 6 - Combining Camera and Lidar/Creating 3D-Objects/cluster_with_roi/src/cluster_with_roi.cpp
+++ Lesson 6 - Combining Camera and Lidar/Creating 3D-Objects/cluster_with_roi/src/cluster_with_roi.cpp
@@ -68,6 +68,62 @@ void showLidarTopview(std::vector<LidarPoint> &lidarPoints, cv::Size worldSize, 
     cv::waitKey(0); // wait for key to be pressed
 }
 
+// Attempt to create show 3D Objects function that only exists in the video
+void show3DObjects(std::vector<BoundingBox> &boundingBoxes, cv::Size worldSize, cv::Size imageSize, bool bWait)
+{
+    // create topview image
+    cv::Mat topviewImg(imageSize, CV_8UC3, cv::Scalar(255, 255, 255));
+
+    for(auto it1=boundingBoxes.begin(); it1!=boundingBoxes.end(); ++it1)
+    {
+        // create randomized color for currrent 3D object
+        cv::RNG rng(it1->boxID);
+        cv::Scalar currColor = cv::Scalar(rng.uniform(0,150), rng.uniform(0, 150), rng.uniform(0, 150));
+
+        // plot Lidar points into top view image
+        int top=1e8, left=1e8, bottom=0.0, right=0.0;
+        float xwmin=1e8, ywmin=1e8, ywmax=-1e8;
+        for (auto it2 = it1->lidarPoints.begin(); it2 != it1->lidarPoints.end(); ++it2)
+        {
+            float xw = (*it2).x; // world position in m with x facing forward from sensor
+            float yw = (*it2).y; // world position in m with y facing left from sensor
+            xwmin = xwmin<xw ? xwmin : xw;
+            ywmin = ywmin<yw ? ywmin : yw;
+            ywmax = ywmax>yw ? ywmax : yw;
+
+            // top-view coordinates
+            int y = (-xw * imageSize.height / worldSize.height) + imageSize.height;
+            int x = (-yw * imageSize.height / worldSize.height) + imageSize.width / 2;
+
+            // find enclosing rectangle
+            top = top<y ? top : y;
+            left = left<x ? left : x;
+            bottom = bottom>y ? bottom : y;
+            right = right>x ? right : x;
+
+            // draw individual point
+            cv::circle(topviewImg, cv::Point(x, y), 4, currColor, -1);
+        }
+
+        // draw enclosing rectangle
+        cv::rectangle(topviewImg, cv::Point(left, top), cv::Point(right, bottom), cv::Scalar(0, 0, 0), 2);
+
+        // augment object with some key data
+        char str1[200], str2[200];
+        sprintf(str1, "id=%d, #pts=%d", it1->boxID, (int)it1->lidarPoints.size());
+        cv::putText(topviewImg, str1, cv::Point2f(left-250, bottom+50), cv::FONT_ITALIC, 2, currColor);
+    }
+
+    string windowName = "3D Objects";
+    cv::namedWindow(windowName, 2);
+    cv::imshow(windowName, topviewImg);
+
+    if(bWait)
+    {
+        cv::waitKey(0);
+    }
+}
+
 // TODO - Add your code inside this function
 void clusterLidarWithROI(std::vector<BoundingBox> &boundingBoxes, std::vector<LidarPoint> &lidarPoints)
 {
@@ -109,15 +165,16 @@ void clusterLidarWithROI(std::vector<BoundingBox> &boundingBoxes, std::vector<Li
             // check wether point is within current bounding box
             if (smallerBox.contains(pt))
             {
-                it2->lidarPoints.push_back(*it1);
-                lidarPoints.erase(it1);
-                it1--;
-                break;
+                enclosingBoxes.push_back(it2);
             }
         } // eof loop over all bounding boxes
         
       // TODO - check wether point has been enclosed by one or by multiple boxes. 
       // Accordingly, add Lidar point to bounding box
+      if (enclosingBoxes.size() == 1)
+      {
+        enclosingBoxes[0]->lidarPoints.push_back(*it1);
+      }
 
     } // eof loop over all Lidar points
 }
@@ -137,7 +194,9 @@ int main()
         {
             showLidarTopview(it->lidarPoints, cv::Size(10.0, 25.0), cv::Size(1000, 2000));
         }
-    }   
+    }
+
+    show3DObjects(boundingBoxes, cv::Size(10.0, 25.0), cv::Size(1000, 2000), true);
 
     return 0;
 }
